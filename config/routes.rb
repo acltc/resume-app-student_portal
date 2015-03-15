725197ea45f22774d23732e9855a301665b66319
Alex Dulchinos <adulchinos@gmail.com>
2015-03-15 14:17:59 -0500

    added routes for educations; continued work on poro

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/student/:id/edit' => 'students#edit'
   get '/skills/:id' => 'skills#show'
   get '/skills/:id/edit' => 'skills#edit'
-
+  get '/education/:id' => 'educations#show'
+  get 'education/:id/edit' => 'educations#edit'
+  
 end
