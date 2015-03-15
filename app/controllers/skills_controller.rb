9c76322aa900b5ef7e0cc3d4ecaed81821184cb3
Alexander Cole <alexanderthecole@gmail.com>
2015-03-15 13:36:16 -0500

    added stuff to skills controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,15 @@
 class SkillsController < ApplicationController
+
+  def index 
+    @skills = Unirest.get("").body
+  end
+
+  def edit 
+
+  end
+
+  def create
+    @skills = Unirest.post("http://localhost:3000/characters.json", :bio => params[:bio]}).body
+    redirect_to _path(@character["id"])
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
+  get '/skills' => 'skills#show'
   get '/student' => 'students#show'
   get '/student/:id/edit' => 'students#edit'
-  get '/skills' => 'skills#show'
   get '/skills/:id/edit' => 'skills#edit'
 
 end
