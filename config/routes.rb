5684d5d591d2aa1d935a5f75a79fd0fff8a22ada
sharonknight <sharon.e.knight@gmail.com>
2015-03-15 13:11:29 -0500

    fixed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/student' => 'students#show'
   get '/student/:id/edit' => 'students#edit'
   get '/skills' => 'skills#show'
-  get '/skills/:id/edit'
+  get '/skills/:id/edit' => 'skills#edit'
 
 end
