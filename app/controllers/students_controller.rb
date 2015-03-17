c00fe146a04695acbf7636355705715e49ff8ffe
sharonknight <sharon.e.knight@gmail.com>
2015-03-16 20:23:59 -0500

    prepopulated edit form fields

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,12 +12,40 @@ class StudentsController < ApplicationController
 	end
 
 	def create
-		  @student = Unirest.post("http://localhost:3000/students.json", :headers => {"Accept" => "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email], :phone_number => params[:phone_number], :linkedin => params[:linkedin], :twitter => params[:twitter], :blog => params[:blog], :online_resume => params[:online_resume], :github => params[:github], :photo => params[:photo], :short_bio => [:short_bio]}).body
+		  @student = Unirest.post("http://localhost:3000/students.json", 
+		  	:headers => {"Accept" => "application/json"}, 
+		  	:parameters => 
+		  	{:first_name => params[:first_name], 
+		  		:last_name => params[:last_name], 
+		  		:email => params[:email], 
+		  		:phone_number => params[:phone_number], 
+		  		:linkedin => params[:linkedin], 
+		  		:twitter => params[:twitter], 
+		  		:blog => params[:blog], 
+		  		:online_resume => params[:online_resume], 
+		  		:github => params[:github], 
+		  		:photo => params[:photo], 
+		  		:short_bio => [:short_bio]}).body
     	  redirect_to student_path(@student["id"])
 	end
 
 	def update
-		redirect_to "/students/:id"
+		@student = Unirest.patch("http://localhost:3000/students/#{params[:id]}.json", 
+			:headers => {"Accept" => "application/json"}, 
+			:parameters => 
+			{:first_name => params[:first_name], 
+				:last_name => params[:last_name], 
+				:email => params[:email], 
+				:phone_number => params[:phone_number], 
+				:linkedin => params[:linkedin], 
+				:twitter => params[:twitter], 
+				:blog => params[:blog], 
+				:online_resume => params[:online_resume], 
+				:github => params[:github], 
+				:photo => params[:photo], 
+				:short_bio => params[:short_bio]}).body
+    	  redirect_to students_path
+
 	end
 
 end
